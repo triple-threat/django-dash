61d3a325b2c22045a0ee9cedb11b181c98c01fb0
Lincoln de Sousa <lincoln@comum.org>
2012-08-19 19:40:53 -0400

    Stuff to show static files with debug set to False

--- urls.py
+++ urls.py
@@ -36,4 +36,8 @@ urlpatterns += patterns('',
     }),
 )
 
-urlpatterns += staticfiles_urlpatterns()
+urlpatterns += patterns('',
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.STATIC_ROOT,
+    }),
+)
