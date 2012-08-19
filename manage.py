71d12d846ee299874e82d12be73de49b708c5a1c
Lincoln de Sousa <lincoln@comum.org>
2012-08-19 00:06:17 -0400

    We've got style in manage.py

--- manage.py
+++ manage.py
@@ -10,4 +10,12 @@ if __name__ == "__main__":
 
     from django.core.management import execute_from_command_line
 
+    if not 'RUN_MAIN' in os.environ:
+        print '''\
+;-.                            .
+|  )               o           |
+|-'  ;-. ,-. ;-.-. . ,-. ,-.   | . .
+|    |   | | | | | | `-. |-'   | | |
+'    '   `-' ' ' ' ' `-' `-' o ' `-|
+                                 `-' '''
     execute_from_command_line(sys.argv)
