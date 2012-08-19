1a030688c8019d859725d513b25cde92a5090354
suneel0101 <suneel0101@gmail.com>
2012-08-19 12:30:04 -0400

    fixed tweet text to take into account if the sharer is the creator or not

--- apps/promise/templatetags/social.py
+++ apps/promise/templatetags/social.py
@@ -12,15 +12,23 @@ def fb():
 
 
 @register.inclusion_tag('parts/social.html')
-def social(promise, redirect_url=None):
+def social(promise, sharer, redirect_url=None):
     redirect_url = redirect_url or promise.get_absolute_url()
     blah = promise.creator.get_avatar_url('large', True)
+
+    if sharer == promise.creator:
+        subject = "my"
+    else:
+        subject = "{}\'s".format(promise.creator.name)
+
+    post_title = u'Support {} promise'.format(subject)
+
     return {
         'app_id': settings.FACEBOOK_APP_ID,
         'app_name': u'Promise.ly',
         'app_icon': promise.creator.get_avatar_url('large', True),
         'link': promise.get_absolute_url(),
-        'post_title': u'Support {}\'s promise'.format(promise.creator.name),
+        'post_title': post_title,
         'post_caption': '',
         'post_description': '"{}"'.format(promise.text),
         'redirect_url': redirect_url,
