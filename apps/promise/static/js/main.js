2d9e8fd8fcd02c5d3796af46e7b1af9a934ba4e5
suneel0101 <suneel0101@gmail.com>
2012-08-19 00:25:33 -0400

    naive natural language processing, modal, promise creation flow

--- apps/promise/static/js/main.js
+++ apps/promise/static/js/main.js
@@ -1,6 +1,60 @@
-// the promise form
-$('.promise-button').click(function() {
-    if ($('#id_text').val()) {
-        $('#promise-modal').modal('show');
-    }
-});
+(function(){
+
+    var numberMap = {
+        next: 1,
+        one: 1,
+        two: 2,
+        three: 3,
+        four: 4,
+        five: 5,
+        six: 6
+    };
+
+    var weekMap = {
+        day: 1,
+        week: 2
+    };
+
+    // the promise form
+    $('.promise-form').submit(function(e) {
+        e.preventDefault();
+
+        var promiseInput = $('#id_text'),
+            promise = promiseInput.val();
+
+        if (promise) {
+
+            // naive natural language processing
+            var mPromise = promise.match(/(\w+)\s*(week|day)\s*/i);
+
+            if (mPromise) {
+                var value = mPromise[1],
+                    unit = mPromise[2];
+
+                if (isFinite(value)) {
+                    value = parseInt(value, 10);
+                } else {
+                    value = parseInt(numberMap[value.toLowerCase()] || 0, 10);
+                }
+
+                if (value && value < 7) {
+                    $('#promise-duration-value').val(value);
+                }
+
+                unit = unit.toLowerCase();
+                $('#promise-duration-unit').val(weekMap[unit]);
+            }
+
+            $('#promise-modal').modal('show');
+        } else {
+            var controlGroup = $(this);
+            promiseInput.focus();
+
+            controlGroup.addClass('error');
+            setTimeout(function() {
+                controlGroup.removeClass('error');
+            }, 1600);
+        }
+    });
+
+}());
