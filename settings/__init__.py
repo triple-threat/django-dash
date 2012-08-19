fddb0f2e4d97053485fa022b32685e94701f8548
Lincoln de Sousa <lincoln@comum.org>
2012-08-19 19:47:53 -0400

    Setting STATIC_ROOT and changing logic in the url files to provide static data in both environments (debug on/off)

--- settings/__init__.py
+++ settings/__init__.py
@@ -1,7 +1,7 @@
 # Django settings for promisely project.
 import os
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -49,7 +49,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(os.path.dirname(__file__), '..', 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
--- urls.py
+++ urls.py
@@ -36,7 +36,7 @@ urlpatterns += patterns('',
     }),
 )
 
-urlpatterns += patterns('',
+urlpatterns += staticfiles_urlpatterns() if settings.DEBUG else patterns('',
     url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
         'document_root': settings.STATIC_ROOT,
     }),
