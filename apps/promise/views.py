3dd640d868c92b19b0d15400b45857077f58dac5
suneel0101 <suneel0101@gmail.com>
2012-08-28 19:44:40 -0400

    maybe?

--- apps/promise/views.py
+++ apps/promise/views.py
@@ -75,7 +75,7 @@ class ValidatePromise(View):
         # Posting to facebook
         post_data = social(promise, self.request.user.profile)
         supporters = promise.supporter.all()
-        supporter_names = ["@[{}:1:{".format(s.facebook_id) + "{}".format(s.facebook_name) + "}]" for s in supporters if s.facebook_name]
+        supporter_names = ["@[{}]".format(s.facebook_id) for s in supporters]
         msg = (u'I just achieved my promise on Promise.ly: '
                u'{post_description}. Thanks, {} for your support! {}').format(supporter_names, promise.get_absolute_url())
         self.request.user.profile.wall_post(
